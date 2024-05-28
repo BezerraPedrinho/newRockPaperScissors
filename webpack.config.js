30ac65c55be2dac9d512fb83ac6b355a6e7ca3be
Jhonny Michel <phdesign9@gmail.com>
2024-05-28 14:26:29 -0300

    feat(html-context): adding webpack and git ignore

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+let config = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      minify: false,
+    }),
+  ],
+};
+module.exports = config;
